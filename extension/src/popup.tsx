c4ee0f2c68c44267d0f30ee82a4b940481e09b8b
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-08-01 11:06:59 +0200

    extract route paths

--- extension/src/popup.tsx
+++ extension/src/popup.tsx
@@ -1,12 +1,15 @@
 import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import { MemoryRouter, Routes, Route } from 'react-router-dom'
+import { RoutePaths } from './routes/paths'
 import { Provider } from 'react-redux'
 import { TamaguiProvider, Theme } from 'tamagui'
 import tamaguiConfig from './tamagui.config'
 import store from './store'
 
 const Home = lazy(() => import('./routes/Home'))
+const OnboardingStart = lazy(() => import('./routes/onboarding/Start'))
+const OnboardingEnterAddress = lazy(() => import('./routes/onboarding/EnterAddress'))
 
 const App = () => (
   <Provider store={store}>
@@ -15,7 +18,9 @@ const App = () => (
         <Suspense fallback={<div>Loading...</div>}>
           <MemoryRouter>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path={RoutePaths.HOME} element={<Home />} />
+              <Route path={RoutePaths.ONBOARDING_START} element={<OnboardingStart />} />
+              <Route path={RoutePaths.ONBOARDING_ENTER_ADDRESS} element={<OnboardingEnterAddress />} />
             </Routes>
           </MemoryRouter>
         </Suspense>
---
+++ extension/src/routes/onboarding/EnterAddress.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function EnterAddress() {
+  return (
+    <div>
+      <label>
+        Enter your Safe or signer address
+        <input type="text" />
+      </label>
+    </div>
+  )
+}
