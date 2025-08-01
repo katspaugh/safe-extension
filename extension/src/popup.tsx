c118d760bef030c5f5a4c0638a76501f7baad248
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-08-01 10:50:17 +0200

    Add routing with Redux and fix build path (#5)
    * Add routing and Redux setup
    
    * Use MemoryRouter in popup

--- extension/src/popup.tsx
+++ extension/src/popup.tsx
@@ -1,14 +1,27 @@
+import React, { Suspense, lazy } from 'react'
+import ReactDOM from 'react-dom/client'
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { TamaguiProvider, Theme } from 'tamagui'
 import tamaguiConfig from './tamagui.config'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import store from './store'
+
+const Home = lazy(() => import('./routes/Home'))
 
 const App = () => (
-  <TamaguiProvider config={tamaguiConfig}>
-    <Theme name="light">
-      <h1>Hello from TamagUI</h1>
-    </Theme>
-  </TamaguiProvider>
+  <Provider store={store}>
+    <TamaguiProvider config={tamaguiConfig}>
+      <Theme name="light">
+        <Suspense fallback={<div>Loading...</div>}>
+          <MemoryRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </MemoryRouter>
+        </Suspense>
+      </Theme>
+    </TamaguiProvider>
+  </Provider>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement)
