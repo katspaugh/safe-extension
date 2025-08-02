7ee7ebb79d55a2b7397b138cc11df38865d6d771
katspaugh <381895+katspaugh@users.noreply.github.com>
2025-08-02 13:14:01 +0200

    feat: style routes with TamagUI grid

--- extension/src/routes/Home.tsx
+++ extension/src/routes/Home.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { RoutePaths } from './paths'
+import { Stack, H1, Button } from 'tamagui'
 
 export default function Home() {
   const navigate = useNavigate()
   return (
-    <div>
-      <h1>Welcome to Safe Extension</h1>
-      <button onClick={() => navigate(RoutePaths.ONBOARDING_START)}>Get started</button>
-    </div>
+    <Stack
+      display="grid"
+      gap="$4"
+      p="$4"
+      width="100%"
+      height="100%"
+      justifyContent="center"
+      alignContent="center"
+    >
+      <H1 textAlign="center">Welcome to Safe Extension</H1>
+      <Button onPress={() => navigate(RoutePaths.ONBOARDING_START)}>Get started</Button>
+    </Stack>
   )
 }
--- extension/src/routes/onboarding/Start.tsx
+++ extension/src/routes/onboarding/Start.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { RoutePaths } from '../paths'
+import { Stack, H2, Button, XStack } from 'tamagui'
 
 export default function Start() {
   const navigate = useNavigate()
   return (
-    <div>
-      <h2>Welcome to Safe</h2>
-      <button onClick={() => navigate(RoutePaths.ONBOARDING_ENTER_ADDRESS)}>Already have a Safe?</button>
-      <button>Create a Safe</button>
-    </div>
+    <Stack
+      display="grid"
+      gap="$4"
+      p="$4"
+      width="100%"
+      height="100%"
+      justifyContent="center"
+      alignContent="center"
+    >
+      <H2 textAlign="center">Welcome to Safe</H2>
+      <XStack space="$2" justifyContent="center">
+        <Button onPress={() => navigate(RoutePaths.ONBOARDING_ENTER_ADDRESS)}>
+          Already have a Safe?
+        </Button>
+        <Button>Create a Safe</Button>
+      </XStack>
+    </Stack>
   )
 }
